ee1326b83de1372adf465cac6b1d4ce978228de3
Sadid Bin Hasan <122972806+SadidSD@users.noreply.github.com>
2025-04-13 19:18:16 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import axios from 'axios';
 import profile from './images/cryparion.png'
 
-const API_URL = 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 function App() {
   const [messages, setMessages] = useState([]);
