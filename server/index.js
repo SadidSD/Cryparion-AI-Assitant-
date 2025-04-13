283f7432e4c5fce6255256f0c69e49764ea05035
Sadid Bin Hasan <122972806+SadidSD@users.noreply.github.com>
2025-04-13 20:24:16 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,9 +6,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: 'https://cryparion-ai-assitant.vercel.app/'
-}));
+app.use(cors());
 app.use(express.json());
 
 const USER_INFO = {
