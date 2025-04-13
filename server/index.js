793e8f89cb795d775adeb0b915aa59a8d856aa4d
Sadid Bin Hasan <122972806+SadidSD@users.noreply.github.com>
2025-04-13 20:22:14 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000'
+  origin: 'https://cryparion-ai-assitant.vercel.app/'
 }));
 app.use(express.json());
 
